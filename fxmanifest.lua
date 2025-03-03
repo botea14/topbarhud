621682b17f976adb0748e9f7688f766310e6949c
botea14 <botea14@gmail.com>
2025-03-03 04:48:08 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,8 @@ author 'DriftX'
 description 'TopBar HUD – Clean & Minimalistic Server UI'
 version '1.0.0'
 
+lua54 'yes'
+
 server_scripts {
     'server/main.lua',
 }
@@ -20,4 +22,9 @@ files {
     'html/hud.html',
     'html/hud.css',
     'html/hud.js'
+}
+
+escrow_ignore {
+  "*.lua",
+  "**/*.lua"
 }
